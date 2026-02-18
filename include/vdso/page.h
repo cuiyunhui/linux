7445c40244aacfd71c931e23818d81c246dcd0dd
Kiryl Shutsemau (Meta) <kas@kernel.org>
2026-02-18 16:09:41 +0000

    mm: Introduce PTE_SIZE, PTE_SHIFT and PTE_MASK
    PAGE_SIZE has double meaning: the size of the order-0 allocation from
    the page allocator and the size of virtual address space covered by the
    PTE entry.
    
    Add new defines that specify PTE size, shift and mask.
    
    Signed-off-by: Kiryl Shutsemau (Meta) <kas@kernel.org>

--- include/vdso/page.h
+++ include/vdso/page.h
@@ -5,27 +5,30 @@
 #include <uapi/linux/const.h>
 
 /*
- * PAGE_SHIFT determines the page size.
+ * PTE_SHIFT determines the MMU page size.
  *
- * Note: This definition is required because PAGE_SHIFT is used
+ * Note: This definition is required because PTE_SHIFT is used
  * in several places throughout the codebase.
  */
-#define PAGE_SHIFT      CONFIG_PTE_SHIFT
+#define PTE_SHIFT      CONFIG_PTE_SHIFT
 
-#define PAGE_SIZE	(_AC(1,UL) << CONFIG_PTE_SHIFT)
+#define PTE_SIZE	(_AC(1,UL) << CONFIG_PTE_SHIFT)
 
 #if !defined(CONFIG_64BIT)
 /*
  * Applies only to 32-bit architectures.
  *
  * Subtle: (1 << CONFIG_PTE_SHIFT) is an int, not an unsigned long.
- * So if we assign PAGE_MASK to a larger type it gets extended the
+ * So if we assign PTE_MASK to a larger type it gets extended the
  * way we want (i.e. with 1s in the high bits) while masking a
  * 64-bit value such as phys_addr_t.
  */
-#define PAGE_MASK	(~((1 << CONFIG_PTE_SHIFT) - 1))
+#define PTE_MASK	(~((1 << CONFIG_PTE_SHIFT) - 1))
 #else
-#define PAGE_MASK	(~(PAGE_SIZE - 1))
+#define PTE_MASK	(~(PTE_SIZE - 1))
 #endif
 
+#define PAGE_SHIFT	PTE_SHIFT
+#define PAGE_SIZE	PTE_SIZE
+#define PAGE_MASK	PTE_MASK
 #endif	/* __VDSO_PAGE_H */
