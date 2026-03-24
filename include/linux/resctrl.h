27c4009427e44b20648f30a435f60c89e6be01fc
Yunhui Cui <cuiyunhui@bytedance.com>
2026-03-24 10:38:14 +0800

    resctrl: add IO allocation arch hook prototypes
    Add missing declarations for resctrl_arch_get_io_alloc_enabled() and
    resctrl_arch_io_alloc_enable() to avoid -Wmissing-prototypes warnings.
    
    Signed-off-by: Yunhui Cui <cuiyunhui@bytedance.com>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -484,6 +484,9 @@ static inline u32 resctrl_get_config_index(u32 closid,
 bool resctrl_arch_get_cdp_enabled(enum resctrl_res_level l);
 int resctrl_arch_set_cdp_enabled(enum resctrl_res_level l, bool enable);
 
+bool resctrl_arch_get_io_alloc_enabled(struct rdt_resource *r);
+int resctrl_arch_io_alloc_enable(struct rdt_resource *r, bool enable);
+
 /**
  * resctrl_arch_mbm_cntr_assign_enabled() - Check if MBM counter assignment
  *					    mode is enabled.
