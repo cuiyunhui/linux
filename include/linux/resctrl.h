e024812de8fd84796a14c5039bd46fb31d860daf
Yunhui Cui <cuiyunhui@bytedance.com>
2026-09-05 19:46:13 +0800

    resctrl: Allow range resources to default to minimum
    Some range-based resources represent reservations whose sum is
    constrained by hardware. Initializing every new control group to the
    maximum value can exhaust that shared budget.
    
    Add a resource flag that selects the minimum value as the default for
    new groups. Existing resources retain the current maximum-value default.
    
    Signed-off-by: Yunhui Cui <cuiyunhui@bytedance.com>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -248,6 +248,9 @@ enum membw_throttle_mode {
  * struct resctrl_membw - Memory bandwidth allocation related data
  * @min_bw:		Minimum memory bandwidth percentage user can request
  * @max_bw:		Maximum memory bandwidth value, used as the reset value
+ * @default_to_min:	Use @min_bw instead of @max_bw as the default for new
+ *			groups. This supports sum-constrained reservation
+ *			resources.
  * @bw_gran:		Granularity at which the memory bandwidth is allocated
  * @delay_linear:	True if memory B/W delay is in linear scale
  * @arch_needs_linear:	True if we can't configure non-linear resources
@@ -259,6 +262,7 @@ enum membw_throttle_mode {
 struct resctrl_membw {
 	u32				min_bw;
 	u32				max_bw;
+	bool				default_to_min;
 	u32				bw_gran;
 	u32				delay_linear;
 	bool				arch_needs_linear;
@@ -409,7 +413,7 @@ static inline u32 resctrl_get_default_ctrl(struct rdt_resource *r)
 	case RESCTRL_SCHEMA_BITMAP:
 		return BIT_MASK(r->cache.cbm_len) - 1;
 	case RESCTRL_SCHEMA_RANGE:
-		return r->membw.max_bw;
+		return r->membw.default_to_min ? r->membw.min_bw : r->membw.max_bw;
 	}
 
 	return WARN_ON_ONCE(1);
